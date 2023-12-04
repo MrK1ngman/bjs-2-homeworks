2f1f97c136eab906e7111ea54f2769c9cb02d425
MrK1ngman <97036370+MrK1ngman@users.noreply.github.com>
2023-12-04 23:13:48 +0700

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
+let student1 = new Student("Vasiliy", "male", 19);
+let student2 = new Student("Ivan", "male", 22);
+let student3 = new Student("Anna", "female", 18);
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
-
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (this.marks === undefined) {
+		console.log("студент отчислен");
+	} else {
+		this.marks.push(...marksToAdd);
+	}
 }
-
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks.length = 0) {
+		return 0;
+	} else {
+		let result = 0;
+		for (let i = 0; i < this.marks.length; i++) {
+			result += this.marks[i];
+		}
+		return result / this.marks.length;
+	}
 }
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	this.reason = reason;
+	delete this.subject;
+	delete this.marks;
 }
