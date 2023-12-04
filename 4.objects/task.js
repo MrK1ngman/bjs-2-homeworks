87e58e1970f2cca82388f0f939fb9e0a4f7de5ed
MrK1ngman <97036370+MrK1ngman@users.noreply.github.com>
2023-12-05 00:07:04 +0700

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,6 +11,8 @@ Student.prototype.setSubject = function (subjectName) {
 Student.prototype.addMarks = function(...marksToAdd) {
   if (this.marks !== undefined) {
     this.marks.push(...marksToAdd);
+  } else {
+    return this.marks;
   }
 }
 Student.prototype.getAverage = function() {
