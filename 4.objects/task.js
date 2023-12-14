0a7683a848c065acfabbc8801ee4a19123559ee8
MrK1ngman <97036370+MrK1ngman@users.noreply.github.com>
2023-12-14 23:31:15 +0700

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,30 +1,30 @@
-function Student(name, gender, age){
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.marks = [];
+function Student(name, gender, age) {
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 Student.prototype.addMarks = function(...marksToAdd) {
-  if (this.marks !== undefined) {
-    this.marks.push(...marksToAdd);
-  } else {
-    return this.marks;
-  }
+	if (this.marks !== undefined) {
+		this.marks.push(...marksToAdd);
+	} else {
+		return this.marks;
+	}
 }
 Student.prototype.getAverage = function() {
-  if (this.marks.length > 0) {
-    let result = this.marks.reduce((acc, marks) => acc + marks, 0);
-    return result / this.marks.length;
-  } else {
-    return 0;
-  }
+	if (this.marks.length > 0) {
+		let result = this.marks.reduce((acc, marks) => acc + marks, 0);
+		return result / this.marks.length;
+	} else {
+		return 0;
+	}
 }
 Student.prototype.exclude = function(reason) {
-  delete this.subject;
-  delete this.marks;
-  this.reason = reason;
+	delete this.subject;
+	delete this.marks;
+	this.reason = reason;
 }
