f5ebf2a105816ae64a242c0e0b86b4da561f7939
MrK1ngman <ilya.razin@mail.ru>
2024-04-08 17:57:07 +0700

    1 complete

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,14 @@
-﻿
+﻿function parseCount(value){
+    if(Number.parseFloat(value) === NaN){
+        throw new Error("Невалидное значение");
+    }
+    return Number.parseFloat(value);
+}
+
+function validateCount(value){ 
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
