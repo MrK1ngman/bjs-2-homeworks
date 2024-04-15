7d54d7c5494d5432ec0dcc5249d8bf63ffeca438
MrK1ngman <ilya.razin@mail.ru>
2024-04-15 17:27:32 +0700

    complete 2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,8 +1,9 @@
 ﻿function parseCount(value){
-    if(Number.parseFloat(value) === NaN){
+    let result = Number.parseFloat(value);
+    if(Number.isNaN(result)){
         throw new Error("Невалидное значение");
     }
-    return Number.parseFloat(value);
+    return result;
 }
 
 function validateCount(value){ 
@@ -10,5 +11,25 @@ function validateCount(value){
         return parseCount(value);
     } catch (error) {
         return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if((a + b <= c) || (a + c <= b) || (c + b <= a)){
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    get perimeter() {
+        let perimeter = this.a + this.b + this.c;
+        return perimeter;
+    }
+    get area() {
+        let p = this.perimeter * 0.5;
+        let area = Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c)).toFixed(3);
+        return area;
     }
 }
