2607f126acdae6ee34c5fbdb4be3f02651bf3a1a
MrK1ngman <97036370+MrK1ngman@users.noreply.github.com>
2024-06-09 18:53:33 +0700

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,22 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cash = [];
+    function wrapper(...args) {
+        const hash = md5(...args);
+        let objectInCash = cash.find((item) => item = hash)
+        if (!objectInCash) {
+            console.log('Из кэша:' + objectInCash);
+            return 'Из кэша:' + objectInCash;
+        }
+        let result = func(...args);
+        cash.push(hash);
+        if (cash.length > 5) {
+            cash.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
